f838f7ffd4359417464c1dc0e91ae68fe0ec6bc6
unknown <samqian2@gmail.com>
2018-11-17 12:14:44 -0800

    Initial commit

---
+++ Section4/hello-world.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello world")
+	foo()
+	for i := 0; i < 100; i++{
+		if i % 2 == 0{
+			fmt.Println(i)
+		}
+	}
+	boo()
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func boo() {
+	fmt.Println("and then exit")
+}
